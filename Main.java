f30533d993ad62d44b41dd3c599a545ada83ca79
Lekkas <104061671+LekkasGit@users.noreply.github.com>
2023-01-22 15:48:19 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] argsd) {
+        Superhero spiderman = new Superhero("Spiderman", "Wrist web-shooters to shoot spider web material", 10);
+        Superhero batman = new Superhero("Batman", "Night vision", 10);
+        Superhero superman = new Superhero("Superman", "Can fly", 20);
+        
+        spiderman.run();
+    }   
+}
